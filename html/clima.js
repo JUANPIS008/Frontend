9fd846f5e3e9e6b19445cefeebae40da60aa1d74
JUANPIS008 <juanjosenarvaezortiz@gmail.com>
2025-06-12 16:44:40 -0500

    Se sube la aplicacion del api de clima

---
+++ html/clima.js
@@ -0,0 +1,21 @@
+
+document.getElementById('climaForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const ciudad = document.getElementById('ciudad').value;
+    fetch(`http://localhost:5000/weather?city=${ciudad}`)
+    .then(response => response.json())
+    .then(data => {
+        if (data.weather) {
+            const clima = data.weather[0].description;
+            const temp = data.main.temp;
+            document.getElementById('resultadoClima').innerHTML = `
+            <p><strong>Clima:</strong> ${clima}</p>
+            <p><strong>Temperatura:</strong> ${temp} °C</p>`;
+        } else {
+            document.getElementById('resultadoClima').innerHTML = `<p>No se pudo obtener el clima.</p>`;
+        }
+    })
+    .catch(() => {
+        document.getElementById('resultadoClima').innerHTML = `<p>Error al consultar el clima.</p>`;
+    });
+});
